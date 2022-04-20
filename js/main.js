a63240c4799b6c30ac42e0a81e5f2c0a78198c9a
FRUHD <99921514+FRUHD@users.noreply.github.com>
2022-04-19 20:40:33 -0700

    make img or iframe visible/not; responsive css

--- js/main.js
+++ js/main.js
@@ -3,23 +3,26 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
     const choice = document.querySelector('input').value
-    console.log(choice)
+    console.log(choice)  // log to make sure we're getting the date
   
     const url = `https://api.nasa.gov/planetary/apod?api_key=ZH0CTeMZSuYqX9kfVXgbWXNyIEiBe3jP2p0fMFrr&date=${choice}`
-  
-1:24:03
 
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
           console.log(data)
           if (data.media_type === 'image') {
+              document.querySelector('iframe').style.display = 'none'
+              document.querySelector('img').style.display = ''
               document.querySelector('img').src = data.hdurl
           } else if (data.media_type === 'video') {
+              document.querySelector('img').style.display = 'none'
+              document.querySelector('iframe').style.display = ''
               document.querySelector('iframe').src = data.url
           }
   
-          document.querySelector('h3').innerText = data.explanation
+          document.querySelector('h2').innerText = data.title
+          document.querySelector('.desc').innerText = data.explanation
         })
         .catch(err => {
             console.log(`error ${err}`)
